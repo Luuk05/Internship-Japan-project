5c319e797e4a7b87c6abf8fc2e78a5ae3f77d3eb
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2021-04-23 19:15:29 +0200

    Begin home page website

--- index.php
+++ index.php
@@ -5,18 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Arvo&family=Lato&display=swap" rel="stylesheet">
     <title>Internship Japan</title>
 </head>
 <body>
     <div class="container">
         <nav>
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">For companies</a></li>
-                <li><a href="">About</a></li>
+                <img src="images/internshiplogo.png" alt="Intership Japan">
+                <li><a href="">For Employers</a></li>
+                <li><a href="">About us</a></li>
+                <li><a href="">F.A.Q.</a></li>
             </ul>
+            <button>Sign in/up</button>
         </nav>
+        <h1 class="home-h1">Internship in Japan</h1>
+        <h2 class="home-h2">Your journey starts here!</h2>
+        <form action="" method="post">
+            <input type="text" placeholder="Opleiding..." name="opleiding" class="input-veld-opleiding input-algemeen">
+            <input type="text" placeholder="locatie..." name="locatie" class="input-veld-locatie input-algemeen">
+            <input type="submit" name="go-knop" value="Go!" class="goknop">
+            <br/>
+            <input type="submit" name="knopvoorstel1" value="knopvoorstel" class="voorstel-stage-knop">
+            <input type="submit" name="knopvoorstel2" value="knopvoorstel" class="voorstel-stage-knop">
+            <input type="submit" name="knopvoorstel3" value="knopvoorstel" class="voorstel-stage-knop">
+            <input type="submit" name="knopvoorstel4" value="knopvoorstel" class="voorstel-stage-knop">
+        </form>
     </div>
-    <h1>test</h1>
+    
 </body>
 </html>
