b79650ab9a321c43a53128035a4092b279679f0a
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2021-04-26 12:07:33 +0200

    kleine visuele aanpassingen

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Arvo&family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style1.css">
     <title>Internship Japan</title>
 </head>
 <body>
@@ -22,25 +22,23 @@
             </ul>
             <button>Sign in/up</button>
         </nav>
-        <div class="een-geheel">
-            <h1 class="home-h1">Internship in Japan</h1>
-            <h2 class="home-h2">Your journey starts here!</h2>
-            <form action="" method="post">
-                <div class="rij1">
-                    <div class="input-velden rij1-voorstel-knoppen">
-                        <input type="text" placeholder="Opleiding..." name="opleiding" class="input-veld-opleiding input-algemeen">
-                        <input type="text" placeholder="locatie..." name="locatie" class="input-veld-locatie input-algemeen">
-                    </div>
-                    <input type="submit" name="go-knop" value="Go!" class="go-knop">
+        <h1 class="home-h1">Internship in Japan</h1>
+        <h2 class="home-h2">Your journey starts here!</h2>
+        <form action="" method="post">
+            <div class="rij1">
+                <div class="input-velden">
+                    <input type="text" placeholder="Opleiding..." name="opleiding" class="input-veld-opleiding input-algemeen">
+                    <input type="text" placeholder="locatie..." name="locatie" class="input-veld-locatie input-algemeen">
                 </div>
-                <div class="rij2">
-                    <input type="submit" name="knopvoorstel1" value="Knop-voorstel-stage" class="voorstel-stage-knop rij1-voorstel-knoppen">
-                    <input type="submit" name="knopvoorstel2" value="Knop-voorstel-stage" class="voorstel-stage-knop rij1-voorstel-knoppen">
-                    <input type="submit" name="knopvoorstel3" value="Knop-voorstel-stage" class="voorstel-stage-knop rij1-voorstel-knoppen">
-                    <input type="submit" name="knopvoorstel4" value="Knop-voorstel-stage" class="voorstel-stage-knop rij1-voorstel-knoppen">
-                </div>
-            </form>
-        </div>
+                <input type="submit" name="go-knop" value="Go!" class="go-knop">
+            </div>
+            <div class="rij2">
+                <input type="submit" name="knopvoorstel1" value="Knop-voorstel-stage" class="voorstel-stage-knop">
+                <input type="submit" name="knopvoorstel2" value="Knop-voorstel-stage" class="voorstel-stage-knop">
+                <input type="submit" name="knopvoorstel3" value="Knop-voorstel-stage" class="voorstel-stage-knop">
+                <input type="submit" name="knopvoorstel4" value="Knop-voorstel-stage" class="voorstel-stage-knop">
+            </div>
+        </form>
     </div>
     
 </body>
