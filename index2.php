c53f9e2ae5da17e459ae583516985b10e07c0843
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2021-04-29 20:31:02 +0200

    andere hoofdpagina test

---
+++ index2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Arvo&family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style_index2.css">
+    <title>Internship Japan</title>
+</head>
+<body>
+    <div class="container-1">
+        <nav>
+            <ul>
+                <!-- <img src="images/internshiplogo.png" alt="Intership Japan"> -->
+                <li><a href="">For Employers</a></li>
+                <li><a href="">About us</a></li>
+                <li><a href="">F.A.Q.</a></li>
+            </ul>
+            <button>Sign in/up</button>
+        </nav>
+        <div class="container-2">
+            <div class="box1">
+                <form action="" method="post">
+                    <h2>Search:</h2>
+                    <input type="text" placeholder="Opleiding..." name="opleiding" class="input-veld-opleiding input-algemeen">
+                    <br>
+                    <input type="text" placeholder="Locatie..." name="locatie" class="input-veld-locatie input-algemeen">
+                    <br>
+                    <input type="submit" name="search-knop" value="Search!" class="search-knop">
+                    <div class="sign-box">
+                        <h3>Sign in</h3>
+                        <div class="line"></div>
+                        <h3>Sign up</h3>
+                    </div>
+                    
+                    <!-- <div class="rij2">
+                        <input type="submit" name="knopvoorstel1" value="Knop-voorstel-stage" class="voorstel-stage-knop">
+                        <input type="submit" name="knopvoorstel2" value="Knop-voorstel-stage" class="voorstel-stage-knop">
+                        <input type="submit" name="knopvoorstel3" value="Knop-voorstel-stage" class="voorstel-stage-knop">
+                        <input type="submit" name="knopvoorstel4" value="Knop-voorstel-stage" class="voorstel-stage-knop">
+                    </div> -->
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
