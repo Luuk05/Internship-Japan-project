dd06593f461073dd05067b117aab8e23c688c432
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2021-06-01 17:48:05 +0200

    ajax afwachten promises

---
+++ check_username_exists.php
@@ -0,0 +1,38 @@
+<?php 
+    include_once "pdo_verbinding.php";
+
+    // $_POST = array();
+    // $_POST["userName"] = "Samson";
+    // print_r($_POST);
+
+    if (isset($_POST["userName"])) {
+
+        $userName = $_POST["userName"];
+
+        $stmt = $pdo->query("SELECT * FROM user"); 
+        $stmt->execute();
+    
+        $rows = $stmt->fetchAll();
+                            
+        $userNameExists = false;
+    
+        foreach($rows as $row) {
+            // echo $row["username"];
+            if ($row["username"] == $userName) {
+                $userNameExists = true;
+                break;
+            }
+        }
+    
+        if ($userNameExists) {
+            echo " ";
+        } else {
+            echo $userName;
+        }
+        
+        exit();
+    }
+
+
+
+?>
--- js/validatie_registreer.js
+++ js/validatie_registreer.js
@@ -6,27 +6,83 @@ $("#username").on("input", function() {
 $("#submit-button").on("click", function(event) {
     event.preventDefault();
 
-    if ($("#username").val() != "" ) {
-        console.log("1");
-        var username = $("#username").val();
-        var password = $("#password").val();
+    var userName = validateUserName();
 
+    function validateUserName() {
+        var userName = $("#username").val();
+        validate(userName);  
+    }
+
+    function validate(userName) {
+        var userName2;
         $.ajax({
-            url: "test.php",
+            url: "check_username_exists.php",
             type: "POST",
             data: 
                 {
-                 "name": username,
-                 "password": password
-                },
-            success: function(data) {
-                myObj = JSON.parse(data);
-                console.log(myObj[0]);
-                // console.log(data);
-            },
+                    "userName": userName
+                }
+                ,
+                success: function(data) {
+                    console.log("Succes");
+                    if (data != " ") {
+                        userName2 = data;
+                    } else {
+                        userName2 = "Name already exists";
+                    }
+                }
         });
-    } else {
-        $("#username").css("border-color", "red");
-        $(".box-login").scrollTop(0);
+        console.log(userName2);
+        return userName2;
     }
+    
+    console.log(userName);
+
+
+    
+    // $.ajax({
+    //     url: "check_username_exists.php",
+    //     type: "POST",
+    //     data: 
+    //         {
+    //             "userName": userName
+    //         },
+    //     success: function(data) {
+    //         console.log("Data ophalen voor username gelukt.");
+    //         if (data != " ") {
+    //             userName = data;
+    //         } else {
+    //             userName = "Naam bestaat al";
+    //         }
+    //     }
+    // });
+
+
+
+
+
+
+    // if ($("#username").val() != "" ) {
+    //     console.log("1");
+    //     var username = $("#username").val();
+    //     var password = $("#password").val();
+
+    //     $.ajax({
+    //         url: "test.php",
+    //         type: "POST",
+    //         data: 
+    //             {
+    //              "name": username,
+    //              "password": password
+    //             },
+    //         success: function(data) {
+    //             // myObj = JSON.parse(data);
+    //             // console.log(myObj[0]);
+    //             console.log("Gelukt");
+    //         },
+    //     });
+    // } else {
+    //     $("#username").css("border-color", "red");
+    //     $(".box-login").scrollTop(0);
+    // }
 });
