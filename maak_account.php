9e662a8a461d397f2f04f09d6d292f52d2712f89
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2021-06-04 15:37:28 +0200

    account maken gelukt

--- maak_account.php
+++ maak_account.php
@@ -6,12 +6,32 @@ if (isset($_POST["userName"])) {
     $passWord = $_POST["passWord"];
     $role = $_POST["role"];
 
+    if ($role == 1) {
+        $roleName = "intern";
+    } else if ($role == 2) {
+        $roleName = "company";
+    } else {
+        $roleName = "education";
+    }
+
     $sql = "INSERT INTO `user`(`username`, `password`, `role`) VALUES (:username, :password, :role)";
     $stmt = $pdo->prepare($sql);
     $stmt->execute(["username" => $userName, "password" => $passWord, "role" => $role]);
   
+
+    $sql = "SELECT * FROM user WHERE username like :username AND password like :password";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([":username" => $userName, ":password" => $passWord]);
+    $row = $stmt->fetch();
+
+    $user_id = $row["user_id"];
+    $standard_id = 252;
+    $sql = "INSERT INTO $roleName(user_id, country_id, nationality_id) VALUES (:userid, :countryid, :nationalityid)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([":userid" => $user_id, ":countryid" => $standard_id, ":nationalityid" => $standard_id]);
+
     
-    echo "Account maken gelukt";
+    echo "Account made";
 } else {
     echo "";
     header("Location: registreer.php");
